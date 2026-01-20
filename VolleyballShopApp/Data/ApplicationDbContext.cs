864bf4f2d5995478e88a3398389d8da97a1cf2fe
deer747slow <niki22502@abv.bg>
2026-01-20 12:32:37 +0200

    Initial commit

---
+++ VolleyballShopApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VolleyballShopApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ VolleyballShopApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VolleyballShopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
