65003246fce1146ab2647db2ef9829d7ffe7ead5
deer747slow <niki22502@abv.bg>
2026-02-16 16:56:57 +0200

    .

--- VolleyballShopApp.Core/Contracts/IProductService.cs
+++ VolleyballShopApp.Core/Contracts/IProductService.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using VolleyballShopApp.Infrastructure.Data.Entities;
+
 namespace VolleyballShopApp.Core.Contracts
 {
-    internal interface IProductService
+    public interface IProductService
     {
+        bool Create(string name, int brandId, int categoryId, string picture, int quantity, decimal price, decimal
+            discount);
+
+        bool Update(int productId, string name, int brandId, int categoryId, string picture, int quantity, decimal
+            price, decimal discount);
+
+        List<Product> GetProducts();
+
+        Product GetProductById(int productId);
+
+        bool RemoveById(int dogproductId);
+
+        List<Product> GetProducts(string searchStringCategoryName, string searchStringBrandName);
     }
 }
--- VolleyballShopApp.Core/Services/ProductService.cs
+++ VolleyballShopApp.Core/Services/ProductService.cs
@@ -4,9 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using VolleyballShopApp.Core.Contracts;
+using VolleyballShopApp.Infrastructure.Data;
+using VolleyballShopApp.Infrastructure.Data.Entities;
+
 namespace VolleyballShopApp.Core.Services
 {
-    internal class ProductService
+    public class ProductService : IProductService
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Create(string name, int brandId, int categoryId, string picture, int quantity, decimal price,
+            decimal discount)
+        {
+            Product item = new Product
+            {
+                ProductName = name,
+                Brand = _context.Brands.Find(brandId),
+                Category = _context.Categories.Find(categoryId),
+
+                Picture = picture,
+                Quantity = quantity,
+                Price = price,
+                Discount = discount
+            };
+
+            _context.Products.Add(item);
+            return _context.SaveChanges() != 0;
+        }
+
+        public Product GetProductById(int productId)
+        {
+            return _context.Products.Find(productId);
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> products = _context.Products.ToList();
+            return products;
+        }
+
+        public List<Product> GetProducts(string searchStringCategoryName, string searchStringBrandName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveById(int dogproductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int productId, string name, int brandId, int categoryId, string picture, int quantity, decimal price, decimal discount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
