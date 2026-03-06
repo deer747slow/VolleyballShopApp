7416e74b5cc48f1156c5e84c9cdccc85027ee130
deer747slow <niki22502@abv.bg>
2026-03-06 16:47:04 +0200

    Implementation to order1

--- VolleyballShopApp.Core/Contracts/IOrderService.cs
+++ VolleyballShopApp.Core/Contracts/IOrderService.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using VolleyballShopApp.Infrastructure.Data.Entities;
+
 namespace VolleyballShopApp.Core.Contracts
 {
-    internal interface IOrderService
+    public interface IOrderService
     {
+        bool Create(int productId, string userId, int quantity);
+
+        List<Order> GetOrders();
+
+        List<Order> GetOrdersByUser(string userId);
+
+        Order GetOrderById(int orderId);
+
+        bool RemoveById(int orderId);
+
+        bool Update(int orderId, int productId, string userId, int quantity);
     }
+
 }
