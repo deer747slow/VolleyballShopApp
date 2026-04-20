17866ae2712ed24c9252c15ce8cb9df79bcbf55d
deer747slow <niki22502@abv.bg>
2026-04-20 23:51:26 +0300

    22

---
+++ VolleyballShopApp/Controllers/FavoriteController1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VolleyballShopApp.Infrastructure.Data;
+using VolleyballShopApp.Infrastructure.Data.Entities;
+
+namespace VolleyballShopApp.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public FavoritesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(int productId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var exists = _context.Favorites
+                .Any(f => f.UserId == user.Id && f.ProductId == productId);
+
+            if (!exists)
+            {
+                var favorite = new Favorite
+                {
+                    UserId = user.Id,
+                    ProductId = productId
+                };
+
+                _context.Favorites.Add(favorite);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index", "Products");
+        }
+    }
+}
---
+++ VolleyballShopApp/Models/Favorites/Favorite.cs
@@ -0,0 +1,13 @@
+﻿namespace VolleyballShopApp.Models.Favorites
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
